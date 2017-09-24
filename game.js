bda1a0aa3cf317c7a397c007e736f5f8b6df5d86
itkazmi814 <itkazmi814@gmail.com>
2017-09-23 23:53:49 -0500

    09232018 1153pm

--- game.js
+++ game.js
@@ -1,26 +1,30 @@
 $(document).ready(function() {
 
-var game = {
+	var game = {
 
 	myChar: null, //holds the player's initially selected character
 	enemyChar: null, //holds the enemy the player is currently fighting
-	htmlPikachu: "<p>hello pikachu</p>",
-	htmlLucario: "<p>hello lucario</p>",
-	htmlSuicune: "<p>hello suicune</p>",
-	htmlGardevoir: "<p>hello gardevoir</p>",
+	htmlPikachu: '<button id="pikachu" class="character-btn transparent mx-auto d-block"><img class="img-fluid mx-auto d-block" src="assets/pikachu.png"></button>',
+	htmlLucario: '<button id="lucario" class="character-btn transparent mx-auto d-block img-fluid"><img class="img-fluid mx-auto d-block" src="assets/lucario.png"></button>',
+	htmlSuicune: '<button id="suicune" class="character-btn transparent mx-auto d-block"><img class="img-fluid mx-auto d-block" src="assets/suicune.png"></button>',
+	htmlGardevoir: '<button id="gardevoir" class="character-btn transparent mx-auto d-block"><img class="img-fluid mx-auto d-block" src="assets/gardevoir.png"></button>',
+
 	choseMyChar: false,
 	choseEnemy: false,
 	pikachu: null,
 	lucario: null,
 	suicune: null,
 	gardevoir: null	,
-	charactersArray: [],			
-	
+	charactersArray: [],		
+
+	htmlMyCharStats: '<section class="stat-block mx-auto d-block"><div class="row text-center"><div id="my-name" class="col-md-12"><p> Name here </p></div></div><div class="row text-center"><div id="my-hp" class="col-md-12"> HP here </p></div></div><div class="row text-center"><div id="my-atk" class="col-md-12"><p> ATK here </p></div></div></section>',
+	htmlEnemyStats: '<section class="stat-block mx-auto d-block"><div class="row text-center"><div id="enemy-name" class="col-md-12"><p> Name here </p></div></div><div class="row text-center"><div id="enemy-hp" class="col-md-12"><p> HP here </p></div></div><div class="row text-center"><div id="enemy-atk" class="col-md-12"><p> ATK here </p></div></div></section>',
+
 	initializeCharacters: function () {
 		this.pikachu = new this.characterGenerator("Pikachu",110,6,6,this.htmlPikachu);
-		this.lucario = new this.characterGenerator("Lucario",110,6,6,this.htmlLucario);
-		this.suicune = new this.characterGenerator("Suicune",110,6,6,this.htmlSuicune);
-		this.gardevoir = new this.characterGenerator("Gardevoir",110,6,6,this.htmlGardevoir);
+		this.lucario = new this.characterGenerator("Lucario",120,6,6,this.htmlLucario);
+		this.suicune = new this.characterGenerator("Suicune",130,6,6,this.htmlSuicune);
+		this.gardevoir = new this.characterGenerator("Gardevoir",140,6,6,this.htmlGardevoir);
 
 		this.charactersArray.push(this.pikachu)
 		this.charactersArray.push(this.lucario)
@@ -42,94 +46,138 @@ var game = {
 	characterPressed: function () {
 		//creates the player's character based on the ID of the button pressed
 		//"this" refers to the button pressed by the player at character selection
+		console.log(this)
+		game.selectOpponents(this);
 
-		console.log($(this))
+		if(game.choseMyChar && game.choseEnemy){
+			console.log("you are ready to play with " + game.myChar.name + " and " + game.enemyChar.name)
+			game.fightSetup(); //repositions myChar and enemyChar, displays stats, and attack button
 
+
+
+
+
+
+		}
+		
+	}, // end characterPressed function
+
+	selectOpponents: function (context) {
+		console.log("entering character selection")
 		if(game.choseMyChar === false){ //block runs if you have not picked a character$(this).attr("id") === "pikachu")
 
 			for(var i = 0; i < game.charactersArray.length; i++){
-				if($(this).attr("id") === game.charactersArray[i].name.toLowerCase()){
+				if($(context).attr("id") === game.charactersArray[i].name.toLowerCase()){
 					game.myChar = game.charactersArray[i]
 					game.choseMyChar = true //when true, the else block will run to set enemyChar
 					game.charactersArray[i].chosen = true //marks the character as chosen
 					console.log("your char: " + game.myChar.name)
-				}
-			}
+				} //end if statement
+			} //end myChar for loop
+		}else if (game.choseMyChar === true) { //if you picked myChar, pick the enemy
+			console.log("entering else if")
+			for(var i = 0; i < game.charactersArray.length; i++){
+				if($(context).attr("id") === game.charactersArray[i].name.toLowerCase() && $(context).attr("id") !== game.myChar.name.toLowerCase()){
+					game.enemyChar = game.charactersArray[i]
+					game.choseEnemy = true //when true, the else block will run to set enemyChar
+					game.charactersArray[i].chosen = true //marks the character as chosen
+					console.log("enemy char: " + game.enemyChar.name)
+				} //end if statement
+			} //end enemyChar for loop
+		}else{
+			console.log("You already chose " + game.myChar.name + " as your character.")
+		} //end chosemyChar conditionals
 
+	}, //end selectOpponents function
 
-
-			if($(this).attr("id") === "pikachu"){
-				game.myChar = game.pikachu
-				game.choseMyChar = true; 
-				game.pikachu.chosen = true; 
-				
-				
-
-			}else if($(this).attr("id") === "lucario"){
-				game.myChar = game.lucario
-				game.choseMyChar = true;
-				game.lucario.chosen = true;
-				console.log("your char: " + game.myChar.name)
-
-			}else if($(this).attr("id") === "suicune"){
-				game.myChar = game.suicune
-				game.choseMyChar = true;
-				game.suicune.chosen = true;
-				console.log("your char: " + game.myChar.name)
-
-			}else if($(this).attr("id") === "gardevoir"){
-				game.myChar = game.gardevoir
-				game.choseMyChar = true;
-				game.gardevoir.chosen = true;
-				console.log("your char: " + game.myChar.name)
-
-			} //end choseMyChar === false block
-		} else if(game.choseMyChar === true){ //block runs if you have picked a character - choseMyChar===true
-
-			if($(this).attr("id") === "pikachu" && game.pikachu.chosen === false){
-				game.enemyChar =  game.pikachu;
-				game.choseEnemy = true;
-				game.pikachu.chosen = true;
-
-			}else if($(this).attr("id") === "lucario" && game.lucario.chosen === false){
-				game.enemyChar = game.lucario
-				game.choseEnemy = true;
-				game.lucario.chosen = true;
-
-			}else if($(this).attr("id") === "suicune" && game.suicune.chosen === false){
-				game.enemyChar = game.suicune
-				game.choseEnemy = true;
-				game.suicune.chosen = true;
-
-			}else if($(this).attr("id") === "gardevoir" && game.gardevoir.chosen === false){
-				game.enemyChar = game.gardevoir
-				game.choseEnemy = true;
-				game.gardevoir.chosen = true;
-				
-
-			}else {
-				console.log("You already chose " + game.myChar.name + " as your character.")
-
-			}
-		}//end choseMyChar === true block
-
-	
-		if(game.choseMyChar && game.choseEnemy){
-			console.log("you are ready to play with " + game.myChar.name + " and " + game.enemyChar.name)
-			//call repositioning function
+	attackButtonPressed: function () {
+		//"this" -> $("#attack-button")		
+		game.myCharAttacks();
+		if(game.isCharAlive(game.enemyChar) === false){
+			game.benchEnemyStatus();
+			//select new enemy
+		}
+		game.enemyCharAttacks();
+		if(game.isCharAlive(game.myChar) === false){
+			//LOSE
 		}
 
-	}// end characterPressed function
+	}, //end attackButtonPressed function
+
+	fightSetup: function () {
+		console.log("Entering fightSetup function")
+
+		$("#pos-two").html(this.myChar.html);
+		$("#pos-three").html(this.enemyChar.html);
+
+		$("#pos-one").html(this.htmlMyCharStats);
+		$("#pos-four").html(this.htmlEnemyStats);
+
+		this.updateOnScreenStats();
+
+	}, //end fightSetup 
+
+	updateOnScreenStats: function() {
+
+		$("#my-name").html(this.myChar.name)
+		$("#my-hp").html("HP: " + this.myChar.hp)
+		$("#my-atk").html("ATK: " + this.myChar.atkVal)
+
+		$("#enemy-name").html(this.enemyChar.name)
+		$("#enemy-hp").html("HP: " + this.enemyChar.hp)
+		$("#enemy-atk").html("ATK: " + this.enemyChar.atkVal)
+	},
+
+	myCharAttacks: function () {
+		this.enemyChar.hp -= this.myChar.atkVal //myChar attack enemyChar
+		this.myChar.atkVal += 6; //myChar's atkVal increases
+		this.updateOnScreenStats();
+	}, //end myCharAttacks functions
+	
+	enemyCharAttacks: function () {
+		this.myChar.hp -= this.enemyChar.atkVal
+		this.updateOnScreenStats()
+	},
+
+	isCharAlive: function (char) {
+		if(char.hp > 0){
+			return true
+		}
+		return false;
+	},
+
+	benchEnemyStatus: function () {
+		var posThreeFilled = false;
+		for(var i=0; i<this.charactersArray.length; i++){
+			if(this.charactersArray[i].chosen === false){
+				if(posThreeFilled === false){
+					this.choseEnemy = false //allows you to pick a new enemy at character select screen
+					$("#pos-one").html(this.myChar.html); //puts myChar in pos1
+					$("#pos-three").html(this.charactersArray[i].html); //puts non-chosen enemy on field
+					posThreeFilled = true;
+				}else{
+					$("#pos-four").html(this.charactersArray[i].html); //puts non-chosen enemy on field
+				}
+				
+				$("#pos-two").html(""); //makes space between myChar and remaining enemies
+				// this.enemyChar = null;
+
+			}//end if
+
+		} //end for loop
+
+	} //end benchEnemyStatus
+
+
 
 } //end game object
 
 
 
-	game.initializeCharacters();
-	console.log(game.charactersArray)
-	console.log(game.charactersArray[0])
-	$(".character-btn").on("click",game.characterPressed);
-	
+game.initializeCharacters();
+$(".character-btn").on("click",game.characterPressed);
+$("#attack-button").on("click",game.attackButtonPressed);
+
 
 
 
